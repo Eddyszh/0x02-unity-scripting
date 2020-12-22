6bf05d42ad66eeed5a05fe17854ca92435ae34fc
Eddyszh <30961017+Eddyszh@users.noreply.github.com>
2020-12-22 11:11:21 -0500

    Add unity project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = new Vector3
+            (
+            transform.position.x - player.transform.position.x,
+            transform.position.y - player.transform.position.y,
+            transform.position.z - player.transform.position.z
+            );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private float horizontalMovement;
+    private float verticalMovement;
+    private Rigidbody rb;
+    private int score = 0;
+    public int health = 5;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * speed;
+        verticalMovement = Input.GetAxis("Vertical") * speed;
+        rb.velocity = new Vector3(horizontalMovement, 0f, verticalMovement);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            score++;
+            other.gameObject.SetActive(false);
+            Debug.Log("Score: " + score);
+        }
+
+        if (other.CompareTag("Trap"))
+        {
+            health--;
+            Debug.Log("Health: " + health);
+        }
+
+        if (other.CompareTag("Goal"))
+            Debug.Log("You win!");
+    }
+}
